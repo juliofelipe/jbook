46c6de192dfb691416f4cc592decbde0342c0231
Julio Felipe <julio@informach.com.br>
2022-06-24 06:02:27 -0300

    chore: add prettier

--- src/components/code-editor.tsx
+++ src/components/code-editor.tsx
@@ -1,14 +1,52 @@
-import MonacoEditor from '@monaco-editor/react';
+import { useRef } from 'react';
+import MonacoEditor, { EditorDidMount } from '@monaco-editor/react';
+import prettier from 'prettier';
+import parser from 'prettier/parser-babel';
 
-const CodeEditor = () => {
-    return <MonacoEditor 
-    language="javascript" 
-    height="500px" 
-    theme='dark'
-    options={{
-        wordWrap: 'on'
-    }}
-    />;
+interface CodeEditorProps {
+    initialValue: string;
+    onChange(value: string): void;
+}
+
+const CodeEditor: React.FC<CodeEditorProps> = ({ onChange, initialValue }) => {
+    const editorRef = useRef<any>();
+
+    const onEditorDidMount: EditorDidMount = (getValue: () => string, monacoEditor) => {
+        editorRef.current = monacoEditor;
+        monacoEditor.onDidChangeModelContent(() => {
+            onChange(getValue());
+        });
+
+        monacoEditor.getModel()?.updateOptions({ tabSize: 2});
+    };
+
+    const onFormatClick = () => {
+        const unformatted = editorRef.current.getModel().getValue();
+
+    }
+
+    return (
+        <div>
+            <button onClick={onFormatClick}>Format</button>
+            <MonacoEditor 
+                editorDidMount={onEditorDidMount}
+                value={initialValue}
+                language="javascript" 
+                height="500px" 
+                theme='dark'
+                options={{
+                    wordWrap: 'on',
+                    minimap: { enabled: false },
+                    showUnused: false,
+                    folding: false,
+                    lineNumbersMinChars: 3,
+                    fontSize: 16,
+                    scrollBeyondLastLine: false,
+                    automaticLayout: true,
+                }}
+            />
+    </div>
+    )
 };
 
 export default CodeEditor;
