7a7f7d4f49a4f0416ed4451ae2e691a4c882c94e
Julio Felipe <julio@informach.com.br>
2022-06-24 19:52:12 -0300

    feat: working in resizable window

--- src/components/resizable.tsx
+++ src/components/resizable.tsx
@@ -1,18 +1,37 @@
 import './resizable.css'
-import { ResizableBox } from "react-resizable";
+import { ResizableBox, ResizableBoxProps } from "react-resizable";
 
 interface ResizableProps {
     direction: 'horizontal' | 'vertical';
 }
 
 const Resizable: React.FC<ResizableProps> = ({ direction, children }) => {
-    return <ResizableBox 
-    height={300} 
-    width={300}
-    resizeHandles={['s']}
-    >
+    let resizableProps: ResizableBoxProps
+
+    if (direction === 'horizontal') {
+        resizableProps = {
+            className: 'resize-horizontal',
+            minConstraints: [window.innerWidth * 0.2, Infinity],
+            maxConstraints: [window.innerWidth * 0.75, Infinity],
+            height: Infinity,
+            width: window.innerWidth * 0.75,
+            resizeHandles: ['e'],
+        };
+    } else {
+        resizableProps = {
+            minConstraints: [Infinity, 24],
+            maxConstraints: [Infinity, window.innerHeight * 0.9],
+            height: 300,
+            width: Infinity,
+            resizeHandles: ['s'],
+        };
+    }
+
+    return (
+    <ResizableBox {...resizableProps}>
         {children}
-    </ResizableBox>;
+    </ResizableBox>
+    )
 };
 
 export default Resizable;
