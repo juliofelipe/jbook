901272e840379fad234140d72d01517b3dfc0479
Julio Felipe <julio@informach.com.br>
2022-06-26 06:53:08 -0300

    fix: adjust add code and add text

--- src/state/action-creators/index.ts
+++ src/state/action-creators/index.ts
@@ -1,15 +1,17 @@
 import { ActionType } from "../action-types";
-import { 
-    Action, 
+import {  
     UpdateCellAction, 
     DeleteCellAction, 
     MoveCellAction, 
-    InsertCellBeforeAction, 
+    InsertCellAfterAction, 
     Direction 
 } from "../actions";
 import { CellTypes } from "../cell";
 
-export const updateCell = (id: string, content: string): UpdateCellAction => {
+export const updateCell = (
+        id: string, 
+        content: string
+    ): UpdateCellAction => {
     return {
         type: ActionType.UPDATE_CELL,
         payload: {
@@ -26,7 +28,10 @@ export const deleteCell = (id: string): DeleteCellAction => {
     };
 };
 
-export const moveCell = (id: string, direction: Direction): MoveCellAction => {
+export const moveCell = (
+        id: string, 
+        direction: Direction
+    ): MoveCellAction => {
     return {
         type: ActionType.MOVE_CELL,
         payload: {
@@ -36,9 +41,12 @@ export const moveCell = (id: string, direction: Direction): MoveCellAction => {
     }
 };
 
-export const insertCellBefore = (id: string, cellType: CellTypes): InsertCellBeforeAction => {
+export const insertCellAfter = (
+        id: string | null, 
+        cellType: CellTypes
+    ): InsertCellAfterAction => {
     return {
-        type: ActionType.INSERT_CELL_BEFORE,
+        type: ActionType.INSERT_CELL_AFTER,
         payload: {
             id,
             type: cellType
